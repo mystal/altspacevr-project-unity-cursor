d7c9344c4f969ed4a2ea50cc48a557b3dc3ca2de
Gabriel Martinez <reitaka@gmail.com>
2015-07-12 12:27:55 -0700

    Completed part 1
    Some constants could use a bit more tuning to fit the sample build, but the functionality works.

--- Project/Assets/Altspace/Scripts/SphericalCursorModule.cs
+++ Project/Assets/Altspace/Scripts/SphericalCursorModule.cs
@@ -6,7 +6,7 @@ public class SphericalCursorModule : MonoBehaviour {
 
 	// This is a scale factor that determines how much to scale down the cursor based on its collision distance.
 	public float DistanceScaleFactor;
-	
+
 	// This is the layer mask to use when performing the ray cast for the objects.
 	// The furniture in the room is in layer 8, everything else is not.
 	private const int ColliderMask = (1 << 8);
@@ -24,29 +24,40 @@ public class SphericalCursorModule : MonoBehaviour {
 	private const float MaxDistance = 100.0f;
 
 	// Sphere radius to project cursor onto if no raycast hit.
-	private const float SphereRadius = 1000.0f;
+	private const float SphereRadius = 10.0f;
+
+	// Screen position the cursor is currently at.
+	private Vector3 CursorScreenPos = new Vector3();
 
     void Awake() {
 		Cursor = transform.Find("Cursor").gameObject;
 		CursorMeshRenderer = Cursor.transform.GetComponentInChildren<MeshRenderer>();
         CursorMeshRenderer.GetComponent<Renderer>().material.color = new Color(0.0f, 0.8f, 1.0f);
+		CursorScreenPos.x = Screen.width / 2.0f;
+		CursorScreenPos.y = Screen.height / 2.0f;
     }	
 
-	void Update()
-	{
-		// TODO: Handle mouse movement to update cursor position.
+	void Update() {
+		UpdateCursor();
+	}
 
-		// TODO: Perform ray cast to find object cursor is pointing at.
-		// TODO: Update cursor transform.
-		var cursorHit = new RaycastHit();/* Your cursor hit code should set this properly. */;
+	private void UpdateCursor() {
+		float mouseDx = Input.GetAxisRaw("Mouse X");
+		float mouseDy = Input.GetAxisRaw("Mouse Y");
+		CursorScreenPos.x += mouseDx * Sensitivity;
+		CursorScreenPos.y += mouseDy * Sensitivity;
 
-		// Update highlighted object based upon the raycast.
-		if (cursorHit.collider != null)
-		{
+		// Perform ray cast to find object cursor is pointing at.
+		var ray = Camera.main.ScreenPointToRay(CursorScreenPos);
+		var cursorHit = new RaycastHit();
+		if (Physics.Raycast(ray, out cursorHit, MaxDistance, ColliderMask)) {
+			Cursor.transform.position = ray.GetPoint(cursorHit.distance);
+			float scale = (cursorHit.distance * DistanceScaleFactor + 1.0f) / 2.0f;
+			Cursor.transform.localScale.Set(scale, scale, scale);
 			Selectable.CurrentSelection = cursorHit.collider.gameObject;
-		}
-		else
-		{
+		} else {
+			Cursor.transform.position = ray.GetPoint(SphereRadius);
+			Cursor.transform.localScale = DefaultCursorScale;
 			Selectable.CurrentSelection = null;
 		}
 	}
---
+++ Project/Assets/Altspace/Shaders/CursorShader.shader
@@ -0,0 +1,36 @@
+﻿Shader "Custom/CursorShader" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" "Queue"="Overlay" }
+		ZTest Always
+		
+		Pass {
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			fixed4 _Color;
+            
+            struct vert_input {
+				float4 vertex : POSITION;
+            };
+            
+            struct frag_input {
+				float4 pos : SV_POSITION;
+            };
+			
+			frag_input vert(vert_input i) {
+				frag_input result;
+				result.pos = mul(UNITY_MATRIX_MVP, i.vertex);
+				return result;
+			}
+
+			fixed4 frag(frag_input i) : COLOR {
+				return _Color;
+			}
+			ENDCG
+		}
+	} 
+	FallBack "Diffuse"
+}
